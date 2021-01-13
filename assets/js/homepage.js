efd14245878119d8f0e15850004b90d7f7208423
Tina Nguyen <nguyentinaca@yahoo.com>
2021-01-13 09:48:04 -0800

    fetch API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,11 @@
-var gerUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    //make a resquest to the url 
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
